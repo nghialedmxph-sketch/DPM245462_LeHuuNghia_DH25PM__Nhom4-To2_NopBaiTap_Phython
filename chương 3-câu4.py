1ced8fb1d401210e8e736831e6db7234e9d342b9
nghialedmxph-sketch <nghiale.dmxph@gmail.com>
2025-10-09 10:23:38 +0700

    09/10

---
+++ chương 3-câu4.py
@@ -0,0 +1,31 @@
+#Câu 4: Hãy cho biết kết quả của Boolean Expression
+'''
+Yêu cầu:
+Cho x, y, z = 3, 5, 7. Hãy cho biết kết quả của Boolean Expression
+(a) x == 3
+(b) x < y
+(c) x >= y
+(d) x <= y
+(e) x != y - 2
+(f) x < 10
+(g) x >= 0 and x < 10
+(h) x < 0 and x < 10
+(i) x >= 0 and x < 2
+(j) x < 0 or x < 10
+(k) x > 0 or x < 10
+(l) x < 0 or x > 10
+
+                                  Bài Làm
+(a) True
+(b) True
+(c) False
+(d) True
+(e) False
+(f) True
+(g) True
+(h) False
+(i) False
+(j) True
+(k) True
+(l) False                                  
+'''
